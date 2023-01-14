222dc7ec0900c592b1a3f72b582fcb545ca48c93
Kathleen Ericson <kericson@utm.edu>
2023-01-14 07:34:38 -0600

    initial commit

---
+++ helper.js
@@ -0,0 +1,30 @@
+//counter to help id elements
+let counter = 0;
+let base = "picture"
+const names = [];
+const reduction = 3;
+
+function addGuy(holderID, url) {
+    let outer = document.getElementById(holderID);
+    let newname = base + counter;
+    counter++;
+    names.push(newname);
+    let image = document.createElement('img');
+    image.setAttribute('crossorigin', 'anonymous');
+    let downloadingImage = new Image();
+    const canvas = document.createElement('canvas');
+    const ctx = canvas.getContext('2d');
+    //let text = document.createTextNode(newname);
+    //button.appendChild(text);
+    outer.appendChild(image);
+    downloadingImage.onload=function(){
+	let h = this.height
+	let w = this.width
+	canvas.width = w / reduction;
+	canvas.height = h / reduction;
+	ctx.drawImage(this, 0, 0, canvas.width, canvas.height);
+	const uri = canvas.toDataURL();
+	image.src = uri;
+    };
+    downloadingImage.src=url;
+}
